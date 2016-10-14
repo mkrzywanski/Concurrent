d15ed2801e1839e6937f2807fbc7c2e327d33686
Michał Krzywański <189705@edu.p.lodz.pl>
2016-10-14 23:38:37 +0200

    improvements

--- ConcurrentBanker/src/main/java/pl/lodz/pl/concurrency/ConcurrentBanker/Banker.java
+++ ConcurrentBanker/src/main/java/pl/lodz/pl/concurrency/ConcurrentBanker/Banker.java
@@ -22,55 +22,55 @@ public class Banker {
 	public void meetClient(Client client) {
 		try {
 			semaphore.acquire();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		logger.info("Bank capital before client came: " + currentCapital + " florens.");
-		logger.info("Client " + client.getId() + " entered the bank");
-		
-		if (client.getCurrentLoan() == 0) {
-			int newLoan = Client.getRandomLoan(maxCapital);
+			logger.info("Bank capital before client came: " + currentCapital + " florens.");
+			logger.info("Client " + client.getId() + " entered the bank");
 			
-			if (clientIdWhoBankerIsWaitingFor != null && !clientIdWhoBankerIsWaitingFor.equals(client.getId())) {
-				logger.info("Cannot make the operation because bank is waiting for the money from client with id "
-						+ clientIdWhoBankerIsWaitingFor);
-				if(client.getWantedLoan() == null) {
-					client.setWantedLoan(newLoan);
-				}
-			} else {
-				logger.info("Client wants to take a loan: " + newLoan);
-				if(client.getWantedLoan() != null) {
-					newLoan = client.getWantedLoan();
-				}
-				if (currentCapital >= newLoan) {
-					logger.info("Accepted");
-					currentCapital -= newLoan;
-					client.setCurrentLoan(newLoan);
-					if (client.getRejectedCounter() >= rejectedMaxValue) {
-						clientIdWhoBankerIsWaitingFor = null;
-					}
-					client.setRejectedCounter(0);
-					client.setWantedLoan(null);
-					client.incrementServiceCounter();
-				} else {
-					client.setRejectedCounter(client.getRejectedCounter() + 1);
-					if (client.getRejectedCounter() == rejectedMaxValue) {
-						clientIdWhoBankerIsWaitingFor = client.getId();
-					}
+			if (client.getCurrentLoan() == 0) {
+				int newLoan = Client.getRandomLoan(maxCapital);
+				
+				if (clientIdWhoBankerIsWaitingFor != null && !clientIdWhoBankerIsWaitingFor.equals(client.getId())) {
+					logger.info("Cannot make the operation because bank is waiting for the money from client with id "
+							+ clientIdWhoBankerIsWaitingFor);
 					if(client.getWantedLoan() == null) {
 						client.setWantedLoan(newLoan);
 					}
-					logger.info("Rejected");
+				} else {
+					if(client.getWantedLoan() != null) {
+						newLoan = client.getWantedLoan();
+					}
+					logger.info("Client wants to take a loan: " + newLoan);
+					if (currentCapital >= newLoan) {
+						logger.info("Accepted");
+						currentCapital -= newLoan;
+						client.setCurrentLoan(newLoan);
+						if (client.getRejectedCounter() >= rejectedMaxValue) {
+							clientIdWhoBankerIsWaitingFor = null;
+						}
+						client.setRejectedCounter(0);
+						client.setWantedLoan(null);
+						client.incrementServiceCounter();
+					} else {
+						client.setRejectedCounter(client.getRejectedCounter() + 1);
+						if (client.getRejectedCounter() == rejectedMaxValue) {
+							clientIdWhoBankerIsWaitingFor = client.getId();
+						}
+						if(client.getWantedLoan() == null) {
+							client.setWantedLoan(newLoan);
+						}
+						logger.info("Rejected");
+					}
 				}
+			} else {
+				logger.info("Client with id " + client.getId() + " wants to give back " + client.getCurrentLoan());
+				currentCapital += client.getCurrentLoan();
+				client.setCurrentLoan(0);
 			}
-		} else {
-			logger.info("Client with id " + client.getId() + " wants to give back " + client.getCurrentLoan());
-			currentCapital += client.getCurrentLoan();
-			client.setCurrentLoan(0);
+			logger.info("Bank capital after operation: " + currentCapital + " florens.\n");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			semaphore.release();
 		}
-		logger.info("Bank capital after operation: " + currentCapital + " florens.\n");
-		semaphore.release();
 	}
 
 	public int getMaxCapital() {
