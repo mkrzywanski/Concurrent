4be1926f8096f3bfc0d6724013da29f3c04fe919
Michał Krzywański <189705@edu.p.lodz.pl>
2016-10-14 22:23:49 +0200

    refactor

--- zad1/src/main/java/pl/lodz/pl/concurrency/zad1/Semaphore.java
+++ zad1/src/main/java/pl/lodz/pl/concurrency/zad1/Semaphore.java
@@ -1,21 +1,23 @@
 package pl.lodz.pl.concurrency.zad1;
 
-import org.apache.log4j.Logger;
-
 public class Semaphore {
 	
-	private static final Logger logger = Logger.getLogger(Semaphore.class);
 	private boolean signal = false;
 	
-	public void acquire() {
-		while(this.signal){
-			logger.info("SEMAPHORE WAITING");
+	public synchronized void acquire() {
+		while(this.signal) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
-	    this.signal = true;
+		this.signal = true;
 	}
-
-	public void release() {
+	
+	public synchronized void release(){
 		this.signal = false;
+		notifyAll();
 	}
 
 }
